43fd202b4637ec6414ff997ce40c87cbcd612341
Ash <ash.row@gmail.com>
2018-04-29 07:28:15 -0600

    Got the indicator marks to work Prediction Input values on plots

--- global.R
+++ global.R
@@ -27,32 +27,6 @@ generatePreds <- function(SepalLength = 5.8, SepalWidth = 3, PetalLength = 4.35,
 
 }
 
-
-
-
-# scatterplot of sepal length vs width
-sepalscatter <- ggplot(data=iris,aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + 
-  geom_point(aes(shape=Species), size=1.5) + 
-  scale_color_manual(values = c("orange", "purple", "green")) + 
-  theme(legend.position=c(.1,1),legend.justification=c(1,1)) +
-  geom_smooth(method="loess") +
-  annotate("point", x = mean(iris$Sepal.Length), y = mean(iris$Sepal.Width), size=4, colour = "red")
-
-
-# marginal density of sepal length - plot on top
-sepalplot_top <- ggplot(data=iris,aes(x=Sepal.Length, fill = Species)) + 
-  geom_density(alpha=.5) + 
-  scale_fill_manual(values = c("orange", "purple","green")) + 
-  theme(legend.position = "none")+
-  geom_vline(xintercept = mean(iris$Sepal.Length), col="red", lwd=1, lty=2)
-
-# marginal density of sepal width- plot on the right
-sepalplot_right <- ggplot(data=iris,aes(x=Sepal.Width, fill = Species)) + 
-  geom_density(alpha=.5) + coord_flip() + 
-  scale_fill_manual(values = c("orange", "purple","green")) + 
-  theme(legend.position = "none") +
-  geom_vline(xintercept = mean(iris$Sepal.Width), col="red", lwd=1, lty=2)
-
 # Empty plot
 empty <- ggplot()+geom_point(aes(1,1), color="white") +
   theme(                              
@@ -67,33 +41,5 @@ empty <- ggplot()+geom_point(aes(1,1), color="white") +
     axis.text.y = element_blank(),
     axis.ticks = element_blank()
   )
-
-
-
-
-
-# scatterplot of petal length vs petal width
-petalscatter <- ggplot(data=iris,aes(x=Petal.Length, y=Petal.Width, color=Species)) + 
-  geom_point(aes(shape=Species), size=1.5) + 
-  scale_color_manual(values = c("orange", "purple", "green")) + 
-  theme(legend.position=c(.1,1),legend.justification=c(1,1)) +
-  geom_smooth(method="loess") +
-  annotate("point", x = mean(iris$Petal.Length), y = mean(iris$Petal.Width), size=4, colour = "red")
-
-
-# marginal density of petal length - plot on top
-petalplot_top <- ggplot(data=iris,aes(x=Petal.Length, fill = Species)) + 
-  geom_density(alpha=.5) + 
-  scale_fill_manual(values = c("orange", "purple","green")) + 
-  theme(legend.position = "none") +
-  geom_vline(xintercept = mean(iris$Petal.Length), col="red", lwd=1, lty=2)
-
-
-# marginal density of petal width - plot on the right
-petalplot_right <- ggplot(data=iris,aes(x=Petal.Width, fill = Species)) + 
-  geom_density(alpha=.5) + coord_flip() + 
-  scale_fill_manual(values = c("orange", "purple","green")) + 
-  theme(legend.position = "none") +
-  geom_vline(xintercept = mean(iris$Petal.Width), col="red", lwd=1, lty=2)
 
 
--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 
-server <- function(input, output){
+server <- function(input, output, session){
   
   Preds <- reactive({
     generatePreds(
@@ -10,37 +10,94 @@ server <- function(input, output){
     )
   })
   
-
- 
-  
-  output$sepalplot <- renderPlot({
-    grid.arrange(sepalplot_top, 
-                 empty, 
-                 sepalscatter,
-                 sepalplot_right, 
-                 ncol=2, nrow=2, 
-                 widths=c(4, 1), 
-                 heights=c(1, 4))
-    })
-  
-  
-  
-  output$petalplot <- renderPlot({
-    grid.arrange(petalplot_top, 
-                 empty, 
-                 petalscatter, 
-                 petalplot_right, 
-                 ncol=2, nrow=2, 
-                 widths=c(4, 1), 
-                 heights=c(1, 4))
-  })
-  
   output$pred_table <- DT::renderDataTable({
     Preds() %>%
       datatable() %>%
       formatPercentage(columns = 'preds', digits = 2)
   })
   
+  # scatterplot of sepal length vs sepal width
+  sepalscatter <- reactive({
+    ggplot(data=iris,aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + 
+      geom_point(aes(shape=Species), size=1.5) + 
+      scale_color_manual(values = c("orange", "purple", "green")) + 
+      theme(legend.position=c(.1,1),legend.justification=c(1,1)) +
+      geom_smooth(method="loess") +
+      annotate("point", x = input$SepalLength, y = input$SepalWidth, size=3, colour = "red")
+    })
+  
+  # marginal density of sepal length - plot on top
+  sepalplot_top <- reactive({
+    ggplot(data=iris,aes(x=Sepal.Length, fill = Species)) + 
+      geom_density(alpha=.5) + 
+      scale_fill_manual(values = c("orange", "purple","green")) + 
+      theme(legend.position = "none")+
+      geom_vline(xintercept = input$SepalLength, col="red", lwd=1, lty=2)
+  })
+  
+  # marginal density of sepal width- plot on the right
+  sepalplot_right <- reactive({
+    ggplot(data=iris,aes(x=Sepal.Width, fill = Species)) + 
+      geom_density(alpha=.5) + coord_flip() + 
+      scale_fill_manual(values = c("orange", "purple","green")) + 
+      theme(legend.position = "none") +
+      geom_vline(xintercept = mean(input$SepalWidth), col="red", lwd=1, lty=2)
+  })
+  
+    output$sepalplot <-  renderPlot({grid.arrange(sepalplot_top(), empty, 
+                                                  sepalscatter(),
+                                                  sepalplot_right(), 
+                                                  ncol=2, nrow=2, 
+                                                  widths=c(4, 1), 
+                                                  heights=c(1, 4))
+                                    })
+  
+  
+  
+  
+  
+    
+    # scatterplot of petal length vs petal width
+    petalscatter <- reactive({
+      ggplot(data=iris,aes(x=Petal.Length, y=Petal.Width, color=Species)) + 
+      geom_point(aes(shape=Species), size=1.5) + 
+      scale_color_manual(values = c("orange", "purple", "green")) + 
+      theme(legend.position=c(.1,1),legend.justification=c(1,1)) +
+      geom_smooth(method="loess") +
+      annotate("point", x = mean(input$PetalLength), y = mean(input$PetalWidth), size=3, colour = "red")
+    })
+    
+    
+    # marginal density of petal length - plot on top
+    petalplot_top <- reactive({
+      ggplot(data=iris,aes(x=Petal.Length, fill = Species)) + 
+      geom_density(alpha=.5) + 
+      scale_fill_manual(values = c("orange", "purple","green")) + 
+      theme(legend.position = "none") +
+      geom_vline(xintercept = mean(input$PetalLength), col="red", lwd=1, lty=2)
+    })
+    
+    
+    # marginal density of petal width - plot on the right
+    petalplot_right <- reactive({
+      ggplot(data=iris,aes(x=Petal.Width, fill = Species)) + 
+      geom_density(alpha=.5) + coord_flip() + 
+      scale_fill_manual(values = c("orange", "purple","green")) + 
+      theme(legend.position = "none") +
+      geom_vline(xintercept = mean(input$PetalWidth), col="red", lwd=1, lty=2)
+    })
+
+    
+    output$petalplot <- renderPlot({grid.arrange(petalplot_top(), empty, 
+                                                 petalscatter(), 
+                                                 petalplot_right(), 
+                                                 ncol=2, nrow=2, 
+                                                 widths=c(4, 1), 
+                                                 heights=c(1, 4))
+    
+  })
+  
+
   
   
 }
